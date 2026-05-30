9ada5f6b6f7c19bca2f3f50b01cded7288b44fee
nathan oliveira <nguerra123.ng@gmail.com>
2026-05-30 15:16:31 -0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -13,6 +14,11 @@ func main() {
 	for {
 		fmt.Print("$ ")
 		fmt.Scanf("%s", &command)
+		command = strings.TrimSpace(command)
+		if command == "exit" {
+			break
+		}
+
 		fmt.Printf("%s: command not found\n", command)
 	}
 }
