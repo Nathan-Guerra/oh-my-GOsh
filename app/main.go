66ec400a079dd26a20ad82a3bfe3d8d71b9efb25
nathan oliveira <nguerra123.ng@gmail.com>
2026-06-18 08:49:06 -0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,10 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
+
+	"github.com/codecrafters-io/shell-starter-go/app/builtins"
 )
 
 func main() {
+	const (
+		SPACE_CHAR   = ' '
+		S_QUOTE_CHAR = '\''
+	)
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("$ ")
@@ -30,7 +38,7 @@ func main() {
 		for {
 			if !found_command {
 				for i < len(input) {
-					if input[i] == ' ' {
+					if input[i] == SPACE_CHAR {
 						found_command = true
 						break
 					}
@@ -40,14 +48,28 @@ func main() {
 				}
 			} else {
 				var arg string
+			loop:
 				for i < len(input) {
-					if input[i] == ' ' {
+					switch input[i] {
+					case SPACE_CHAR:
 						i++
-						break
+						break loop
+					case S_QUOTE_CHAR:
+						i++
+						for i < len(input) {
+							if input[i] == S_QUOTE_CHAR {
+								i++
+								break
+							}
+							arg += string(input[i])
+							i++
+						}
+
+					default:
+						arg += string(input[i])
+						i++
 					}
 
-					arg += string(input[i])
-					i++
 				}
 
 				if len(arg) > 0 {
@@ -60,27 +82,22 @@ func main() {
 			}
 		}
 
-		fmt.Println(command_name)
-		fmt.Printf("%v\n", args)
+		command, exists := builtins.Builtins[command_name]
 
-		// params := strings.Split(input, " ")
-		// // args := params[1:]
-		// command, exists := builtins.Builtins[params[0]]
+		if exists {
+			err := command(args)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else if _, err := exec.LookPath(command_name); err == nil {
+			cmd := exec.Command(command_name, args...)
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
 
-		// if exists {
-		// 	err := command(args)
-		// 	if err != nil {
-		// 		fmt.Println(err)
-		// 	}
-		// } else if _, err := exec.LookPath(params[0]); err == nil {
-		// 	cmd := exec.Command(params[0], args...)
-		// 	cmd.Stdin = os.Stdin
-		// 	cmd.Stdout = os.Stdout
-		// 	cmd.Stderr = os.Stderr
-
-		// 	cmd.Run()
-		// } else {
-		// 	fmt.Printf("%s: command not found\n", params[0])
-		// }
+			cmd.Run()
+		} else {
+			fmt.Printf("%s: command not found\n", command_name)
+		}
 	}
 }
